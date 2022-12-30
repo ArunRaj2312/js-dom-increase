438839a570a4a692298d3ee9aa89051898d66654
ArunRaj2312 <arunvijay1043534@gmail.com>
2022-12-29 17:31:51 -1200

    add file

---
+++ main.js
@@ -0,0 +1,10 @@
+function incre(){
+   let a=document.getElementById("val").value;
+   a++;
+   document.getElementById("val").value=a;
+}
+function decre(){
+    let a=document.getElementById("val").value;
+    a--;
+    document.getElementById("val").value=a;
+ }
